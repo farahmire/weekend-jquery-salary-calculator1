0ab3e3acdcb2ca345631f4bb392071039a26952e
Farah Mire <farah_mire@yahoo.com>
2022-12-02 19:28:54 -0600

    table

---
+++ client.js
@@ -0,0 +1,45 @@
+$(document).ready(onReady);
+/// set template array with object values
+let employees = [ 
+{
+    first: "Farah",
+    last: "Mire",
+    id: 828864,
+    title: "Software Engineer",
+    annualSalary: 70000
+},
+{
+    first: "John",
+    last: "Doe",
+    id: 65324,
+    title: "Firefighter",
+    annualSalary: 60000
+},
+{
+    first: "Justin",
+    last: "Louie",
+    id: 20289,
+    title: "Doctor",
+    annualSalary: 15500
+}
+]
+// set on ready function for DOM
+// make click handler events for when add employee button/ delete is pressed
+function onReady(){
+    $('addSubmit').on('click', submitEmployee)
+
+
+}
+
+
+
+/// here id make the function for click event submit
+function submitEmployee(){
+    let addFirst =
+    let addLast =
+    let addID =
+    let addTitle =
+    let addFirst =
+
+
+}
